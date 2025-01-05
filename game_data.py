c9277b5ddcf3037c0f197c2f5294f15934b8d4b3
dave <dawitymersha@gmail.com>
2025-01-05 08:24:16 -0500

    Initial commit

---
+++ game_data.py
@@ -0,0 +1,107 @@
+data = [
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 181,
+        "description": "Actor, producer, and former professional wrestler, also known as 'The Rock'",
+        "country": "United States",
+        "instagram_handle": "@therock"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 174,
+        "description": "Musician, actress, and producer. Known for her hit songs and acting career",
+        "country": "United States",
+        "instagram_handle": "@selenagomez"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 172,
+        "description": "Reality TV personality, businesswoman, and founder of Kylie Cosmetics",
+        "country": "United States",
+        "instagram_handle": "@kyliejenner"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 167,
+        "description": "Reality TV personality, entrepreneur, and founder of SKIMS",
+        "country": "United States",
+        "instagram_handle": "@kimkardashian"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 500,
+        "description": "Argentine professional footballer, widely regarded as one of the greatest players of all time",
+        "country": "Argentina",
+        "instagram_handle": "@leomessi"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 600,
+        "description": "Portuguese professional footballer, record-breaking athlete, and philanthropist",
+        "country": "Portugal",
+        "instagram_handle": "@cristiano"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 183,
+        "description": "Singer, actress, and songwriter, known for her powerful voice and hit albums",
+        "country": "United States",
+        "instagram_handle": "@arianagrande"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 185,
+        "description": "Actress and singer, known for her roles in 'Euphoria' and Marvel’s 'Spider-Man'",
+        "country": "United States",
+        "instagram_handle": "@zendaya"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 290,
+        "description": "Global pop star and songwriter known for hits like 'Peaches' and 'Sorry'",
+        "country": "Canada",
+        "instagram_handle": "@justinbieber"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 270,
+        "description": "Singer-songwriter known for her narrative songs and record-breaking albums",
+        "country": "United States",
+        "instagram_handle": "@taylorswift"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 320,
+        "description": "Singer, actress, and businesswoman. Widely regarded as one of the greatest entertainers of all time",
+        "country": "United States",
+        "instagram_handle": "@beyonce"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 110,
+        "description": "Grammy-winning singer-songwriter known for her unique style and voice",
+        "country": "United States",
+        "instagram_handle": "@billieeilish"
+    },
+    {
+        "name": "Drake",
+        "follower_count": 140,
+        "description": "Rapper, singer, and producer known for his chart-topping hits",
+        "country": "Canada",
+        "instagram_handle": "@champagnepapi"
+    },
+    {
+        "name": "Rihanna",
+        "follower_count": 150,
+        "description": "Singer, actress, and founder of Fenty Beauty",
+        "country": "Barbados",
+        "instagram_handle": "@badgalriri"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 160,
+        "description": "Model and reality TV star, known for her high-profile modeling career",
+        "country": "United States",
+        "instagram_handle": "@kendalljenner"
+    }
+]
---
+++ main.py
@@ -0,0 +1,62 @@
+#import art
+#import random for the array
+#import the game_data for the celebrities information
+
+import art
+import random
+import game_data
+
+#logo for intro
+print(art.logo)
+
+def Celebrity_pick():
+    """pick two random celebrity from the game_data array"""
+    return random.choice(game_data.data)
+
+#compare function
+def compare(num1, num2):
+    """compare two numbers and return a if number 1 is greater or will return b or else """
+    if num1 > num2:
+        return "a"
+    else:
+        return "b"
+
+
+
+def game(celeb_a, celeb_b, score):
+    """Take two celebrity as an input to compare and play the game"""
+    # print the first celebrity information from array, has to be random
+    if score != 0:
+        print(f"You're right! Current score: {score}.")
+
+    celebrityA = celeb_a
+    print(f"Compare A: {celebrityA["name"]}, a {celebrityA["description"]}, from {celebrityA["country"]}.")
+    # print vs logo
+    print(art.vs)
+
+    # print the second celebrity information from array, has to be random
+    # what if the Celebrity_pick same person twice so we can do if else
+    celebrityB = celeb_b
+    while celebrityA == celebrityB:
+        celebrityB = Celebrity_pick()
+    print(f"Compare B: {celebrityB["name"]}, a {celebrityB["description"]}, from {celebrityB["country"]}.")
+
+    # take input from the user A or B
+    result = compare(celebrityA["follower_count"], celebrityB["follower_count"])
+    A_or_B = input("who has more followers? Type 'A' or 'B': ").lower()
+
+
+    if A_or_B == result:
+        # compare if higher or lower if correct keep B and pick random from the array again
+        # increase the score for the player
+        game(celebrityB, Celebrity_pick(), score + 1)
+
+
+
+
+game(Celebrity_pick(), Celebrity_pick(), 0)
+
+
+
+
+
